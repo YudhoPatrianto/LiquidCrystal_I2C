d27f2b84ba08fb57a4a3ab515a5584af269a6fae
YudhoPatrianto <kydh01123@gmail.com>
2025-08-27 00:08:17 +0700

    Add Support for multiple lcd
    Signed-off-by: YudhoPatrianto <kydh01123@gmail.com>

--- LiquidCrystal_I2C.h
+++ LiquidCrystal_I2C.h
@@ -1,12 +1,11 @@
-//YWROBOT
-#ifndef LiquidCrystal_I2C_h
-#define LiquidCrystal_I2C_h
+#ifndef FDB_LIQUID_CRYSTAL_I2C_H
+#define FDB_LIQUID_CRYSTAL_I2C_H
 
 #include <inttypes.h>
-#include "Print.h" 
+#include <Print.h>
 #include <Wire.h>
 
-// commands
+// Commands
 #define LCD_CLEARDISPLAY 0x01
 #define LCD_RETURNHOME 0x02
 #define LCD_ENTRYMODESET 0x04
@@ -16,13 +15,13 @@
 #define LCD_SETCGRAMADDR 0x40
 #define LCD_SETDDRAMADDR 0x80
 
-// flags for display entry mode
+// Flags for display entry mode
 #define LCD_ENTRYRIGHT 0x00
 #define LCD_ENTRYLEFT 0x02
 #define LCD_ENTRYSHIFTINCREMENT 0x01
 #define LCD_ENTRYSHIFTDECREMENT 0x00
 
-// flags for display on/off control
+// Flags for display on/off control
 #define LCD_DISPLAYON 0x04
 #define LCD_DISPLAYOFF 0x00
 #define LCD_CURSORON 0x02
@@ -30,13 +29,13 @@
 #define LCD_BLINKON 0x01
 #define LCD_BLINKOFF 0x00
 
-// flags for display/cursor shift
+// Flags for display/cursor shift
 #define LCD_DISPLAYMOVE 0x08
 #define LCD_CURSORMOVE 0x00
 #define LCD_MOVERIGHT 0x04
 #define LCD_MOVELEFT 0x00
 
-// flags for function set
+// Flags for function set
 #define LCD_8BITMODE 0x10
 #define LCD_4BITMODE 0x00
 #define LCD_2LINE 0x08
@@ -44,7 +43,7 @@
 #define LCD_5x10DOTS 0x04
 #define LCD_5x8DOTS 0x00
 
-// flags for backlight control
+// Flags for backlight control
 #define LCD_BACKLIGHT 0x08
 #define LCD_NOBACKLIGHT 0x00
 
@@ -54,78 +53,64 @@
 
 class LiquidCrystal_I2C : public Print {
 public:
-  LiquidCrystal_I2C(uint8_t lcd_Addr,uint8_t lcd_cols,uint8_t lcd_rows);
-  void begin(uint8_t cols, uint8_t rows, uint8_t charsize = LCD_5x8DOTS );
-  void clear();
-  void home();
-  void noDisplay();
-  void display();
-  void noBlink();
-  void blink();
-  void noCursor();
-  void cursor();
-  void scrollDisplayLeft();
-  void scrollDisplayRight();
-  void printLeft();
-  void printRight();
-  void leftToRight();
-  void rightToLeft();
-  void shiftIncrement();
-  void shiftDecrement();
-  void noBacklight();
-  void backlight();
-  void autoscroll();
-  void noAutoscroll(); 
-  void createChar(uint8_t, uint8_t[]);
-  void createChar(uint8_t location, const char *charmap);
-  // Example: 	const char bell[8] PROGMEM = {B00100,B01110,B01110,B01110,B11111,B00000,B00100,B00000};
-  
-  void setCursor(uint8_t, uint8_t); 
-#if defined(ARDUINO) && ARDUINO >= 100
-  virtual size_t write(uint8_t);
-#else
-  virtual void write(uint8_t);
-#endif
-  void command(uint8_t);
-  void init();
-  void oled_init();
+    LiquidCrystal_I2C(uint8_t lcd_addr, uint8_t lcd_cols, uint8_t lcd_rows, uint8_t charsize = LCD_5x8DOTS);
 
-////compatibility API function aliases
-void blink_on();						// alias for blink()
-void blink_off();       					// alias for noBlink()
-void cursor_on();      	 					// alias for cursor()
-void cursor_off();      					// alias for noCursor()
-void setBacklight(uint8_t new_val);				// alias for backlight() and nobacklight()
-void load_custom_character(uint8_t char_num, uint8_t *rows);	// alias for createChar()
-void printstr(const char[]);
+    void begin();               // Default begin (uses Wire)
+    void begin(TwoWire* wire);  // Overload for custom I2C bus
 
-////Unsupported API functions (not implemented in this library)
-uint8_t status();
-void setContrast(uint8_t new_val);
-uint8_t keypad();
-void setDelay(int,int);
-void on();
-void off();
-uint8_t init_bargraph(uint8_t graphtype);
-void draw_horizontal_graph(uint8_t row, uint8_t column, uint8_t len,  uint8_t pixel_col_end);
-void draw_vertical_graph(uint8_t row, uint8_t column, uint8_t len,  uint8_t pixel_col_end);
-	 
+    void clear();
+    void home();
+    void noDisplay();
+    void display();
+    void noBlink();
+    void blink();
+    void noCursor();
+    void cursor();
+    void scrollDisplayLeft();
+    void scrollDisplayRight();
+    void printLeft();
+    void printRight();
+    void leftToRight();
+    void rightToLeft();
+    void shiftIncrement();
+    void shiftDecrement();
+    void noBacklight();
+    void backlight();
+    bool getBacklight();
+    void autoscroll();
+    void noAutoscroll();
+    void createChar(uint8_t, uint8_t[]);
+    void setCursor(uint8_t, uint8_t);
+    virtual size_t write(uint8_t);
+    void command(uint8_t);
+
+    inline void blink_on() { blink(); }
+    inline void blink_off() { noBlink(); }
+    inline void cursor_on() { cursor(); }
+    inline void cursor_off() { noCursor(); }
+
+    // Compatibility aliases
+    void setBacklight(uint8_t new_val);
+    void load_custom_character(uint8_t char_num, uint8_t *rows);
+    void printstr(const char[]);
+    void forceReset();
 
 private:
-  void init_priv();
-  void send(uint8_t, uint8_t);
-  void write4bits(uint8_t);
-  void expanderWrite(uint8_t);
-  void pulseEnable(uint8_t);
-  uint8_t _Addr;
-  uint8_t _displayfunction;
-  uint8_t _displaycontrol;
-  uint8_t _displaymode;
-  uint8_t _numlines;
-  bool _oled = false;
-  uint8_t _cols;
-  uint8_t _rows;
-  uint8_t _backlightval;
+    void send(uint8_t, uint8_t);
+    void write4bits(uint8_t);
+    void expanderWrite(uint8_t);
+    void pulseEnable(uint8_t);
+
+    uint8_t _addr;
+    uint8_t _displayfunction;
+    uint8_t _displaycontrol;
+    uint8_t _displaymode;
+    uint8_t _cols;
+    uint8_t _rows;
+    uint8_t _charsize;
+    uint8_t _backlightval;
+
+    TwoWire* _wire = &Wire; // Default to Wire, can be overridden
 };
 
-#endif
+#endif // FDB_LIQUID_CRYSTAL_I2C_H
--- README.md
+++ README.md
@@ -1,6 +1,44 @@
-# LiquidCrystal_I2C
+# LiquidCrystal_I2C_Revision
 
-LiquidCrystal Arduino library for I2C LCD displays
+LiquidCrystal Arduino And ESP32 library for I2C LCD displays 
 
-**Status: Archived** 
-This repository has been transfered to GitLab at https://gitlab.com/tandembyte/LCD_I2C
+what's changed?:
+  - `Add Support Multiple LCD (WARNING! Must Different I2C Addresses)`
+
+**How To Use?**
+```c++
+#include "Wire.h"
+#include "LiquidCrystal_I2C.h"
+
+// Create Twowire
+TwoWire my_lcd1 = TwoWire(0);
+TwoWire my_lcd2 = TwoWire(1);
+
+// Properties LCD1
+const int lcd1_sda = 27;
+const int lcd1_scl = 26;
+LiquidCrystal_I2C lcd1(0x26, 16, 2); 
+
+// Properties LCD2
+const int lcd2_sda = 25;
+const int lcd2_scl = 33;
+LiquidCrystal_I2C lcd2(0x27, 20, 4); 
+
+void setup() {
+  my_lcd1.begin(lcd1_sda, lcd1_scl);
+  lcd1.begin(&my_lcd1);
+  lcd1.backlight();
+  lcd1.setCursor(0,0);
+  lcd1.print("Hello LCD1");
+
+  my_lcd2.begin(lcd2_sda, lcd2_scl);
+  lcd2.begin(&my_lcd2);
+  lcd2.backlight();
+  lcd2.setCursor(0,0);
+  lcd2.print("Hello LCD2");
+}
+
+void loop() {
+
+}
+```
